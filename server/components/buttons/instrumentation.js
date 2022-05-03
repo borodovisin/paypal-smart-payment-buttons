963a561632cfc80db37d4f383855b2112c1bd26f
Arturo Riveron Borodovisina <aborodovisina@paypal.com>
2022-05-03 18:01:22 +0200

    feat(split code): split the name functionality

--- server/components/buttons/instrumentation.js
+++ server/components/buttons/instrumentation.js
@@ -5,8 +5,7 @@ import type { ExpressRequest } from '../../types';
 import { ROOT_TRANSACTION_NAME } from './constants';
 
 type SetRootTransactionOptions = {|
-    userIDToken : ?string,
-    clientAccessToken : ?string,
+    name : string,
     rootTxnData : $Shape<{|
         client_id : string,
         sdk_version : string,
@@ -16,24 +15,35 @@ type SetRootTransactionOptions = {|
     |}>
 |};
 
+/**
+ * Get the transaction name base on the user token identifier,
+ * the client token or a default value in the case
+ * the previous one are not define in the request
+ *
+ * @param {string} userIDToken       the user token
+ * @param {string} clientAccessToken the client token
+ * @returns the name of the root transaction
+ */
+export const getRootTransactionName = (userIDToken : mixed,
+    clientAccessToken : mixed) : string => {
+    if (userIDToken) {
+        return ROOT_TRANSACTION_NAME.SMART_BUTTONS_WALLET;
+    } else if (clientAccessToken) {
+        return ROOT_TRANSACTION_NAME.SMART_BUTTONS_VAULT;
+    }
+    return ROOT_TRANSACTION_NAME.SMART_BUTTONS;
+};
+
 export function setRootTransaction(req : ExpressRequest,
-    { userIDToken, clientAccessToken, rootTxnData } : SetRootTransactionOptions) {
+    { name, rootTxnData } : SetRootTransactionOptions) {
     req.model = req.model || {};
     req.model.rootTxn = req.model.rootTxn || {
-        name: '',
         data: {}
     };
 
     const rootTxn = req.model.rootTxn;
     const existingData = req.model.rootTxn.data;
-
-    if (userIDToken) {
-        rootTxn.name = ROOT_TRANSACTION_NAME.SMART_BUTTONS_WALLET;
-    } else if (clientAccessToken) {
-        rootTxn.name = ROOT_TRANSACTION_NAME.SMART_BUTTONS_VAULT;
-    } else {
-        rootTxn.name = ROOT_TRANSACTION_NAME.SMART_BUTTONS;
-    }
+    rootTxn.name = name;
 
     req.model.rootTxn.data = {
         ...existingData,
